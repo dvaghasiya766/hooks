0e000e0456919873979275cc8369be57b66e91ce
Equis Dev <dvaghasiya766@rku.ac.in>
2025-07-30 18:01:26 +0530

    Complete Hook Guidline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-// import UseStateHook from "./components/useState/state";
-// import UseEffectHook from "./components/useEffect/effect";
-// import UseContextHook from "./components/useContext/context";
-// import UseRefHook from "./components/useRef/ref";
-// import UseReducerHook from "./components/useReducer/reducer";
-// import UseLayoutEffectHook from "./components/useLayoutEffect/layoutEffect";
-// import UseMemoHook from "./components/useMemo/memo";
-// import UseCallBackHook from "./components/useCallBack/callBack";
+import UseStateHook from "./components/useState/state";
+import UseEffectHook from "./components/useEffect/effect";
+import UseContextHook from "./components/useContext/context";
+import UseRefHook from "./components/useRef/ref";
+import UseReducerHook from "./components/useReducer/reducer";
+import UseLayoutEffectHook from "./components/useLayoutEffect/layoutEffect";
+import UseMemoHook from "./components/useMemo/memo";
+import UseCallBackHook from "./components/useCallBack/callBack";
 import TestCustomHook from "./components/customHooks/testHooks";
 
 import "./App.css";
@@ -15,14 +15,14 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      {/* <UseStateHook />
+      <UseStateHook />
       <UseEffectHook />
       <UseContextHook />
       <UseRefHook />
       <UseReducerHook />
       <UseLayoutEffectHook />
       <UseMemoHook />
-      <UseCallBackHook /> */}
+      <UseCallBackHook />
       <TestCustomHook />
     </div>
   );
