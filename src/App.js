f5e03aa310fd3c6204868c265aba68ad795241bb
Equis Dev <dvaghasiya766@rku.ac.in>
2025-07-30 11:04:01 +0530

    Initialize React Project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React, { Component } from "react";
+
 import "./App.css";
-import Counter1 from "./components/counter1";
-import StateTutorial from "./components/useState/stateTutorial";
-import Memo from "./components/useMemo/memoTutorial";
-import Parent from "./components/useCallBack/parent";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    };
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount: when component renders first time");
-  }
-
-  increment() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
-        <Counter1 count={this.state.count} />
-        <button
-          className="App-button"
-          onClick={() => this.increment()}
-        >Click me</button>
-        <StateTutorial />
-        <Memo />
-        <Parent />
+        <h1>Hello Word!</h1>
       </div>
     );
   }
